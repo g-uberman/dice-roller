381d175356c81bc78ac40d5ca18322e05a63a078
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-10-10 17:01:41 +0200

    roll button functionality added (static d4 roll)

--- scripts.js
+++ scripts.js
@@ -1,34 +1,91 @@
-const D6input = document.getElementById("D6input")
-const D6plus = document.getElementById("D6plus")
-const D6minus = document.getElementById("D6minus")
+// HOOKS
+
+const D6input = document.getElementById("D6input");
+const D6plus = document.getElementById("D6plus");
+const D6minus = document.getElementById("D6minus");
+const rollResult = document.getElementById("rollResult");
+
+// VARIABLES
 
 let numOfD6 = 0;
 
+// FUNCTIONS
+
 plusDice = function (dice) {
-    switch (dice) {
-        case 'D6':
-            numOfD6++;
-            D6input.value = numOfD6;
-            break;
-        default:
-            console.log('Dice not added yet.')
-    }
-}
+  switch (dice) {
+    case "D6":
+      numOfD6++;
+      D6input.value = numOfD6;
+      break;
+    default:
+      console.log("Dice not added yet.");
+  }
+};
 
 minusDice = function (dice) {
-    switch (dice) {
-        case 'D6':
-            if ( numOfD6 > 0) {
-            numOfD6--;
-            D6input.value = numOfD6;
-            break;
-            } else { return null; };
-        default:
-            console.log('Dice not added yet.')
-    }
+  switch (dice) {
+    case "D6":
+      if (numOfD6 > 0) {
+        numOfD6--;
+        D6input.value = numOfD6;
+        break;
+      } else {
+        return null;
+      }
+    default:
+      console.log("Dice not added yet.");
+  }
+};
+
+// ROLL MECHANIC
+
+let dieResult = 1;
+
+const rollanddisplay = function (sides) {
+    rollDie(sides);
+    displayResult();
+}
+
+const rollDie = function (sides) {
+  dieResult = 1;
+
+  function singleTest() {
+    return new Promise((resolve, reject) => {
+      if (Math.random() < 0.5) {
+        resolve();
+        dieResult++;
+      } else {
+        reject();
+      }
+    });
+  }
+
+  for (let i = 0; i < sides - 1; i++) {
+    singleTest()
+      .then(() => {
+        console.log("rolled");
+      })
+      .catch(() => {
+        return null;
+      });
+  }
+
+};
+
+const displayResult = function () {
+    rollResult.innerText = dieResult;
 }
 
 // BUTTONS
 
-D6plus.addEventListener('click', (e) => {plusDice('D6'); e.preventDefault();});
-D6minus.addEventListener('click', (e) => {minusDice('D6'); e.preventDefault();});
+D6plus.addEventListener("click", (e) => {
+  plusDice("D6");
+  e.preventDefault();
+});
+D6minus.addEventListener("click", (e) => {
+  minusDice("D6");
+  e.preventDefault();
+});
+roll.addEventListener("click", () => {
+    rollanddisplay(4);
+});

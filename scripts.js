27244c578712c880cf81566c3fd2dbfcef814fd6
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-10-11 13:11:16 +0200

    added js hooks to all dice forms

--- scripts.js
+++ scripts.js
@@ -1,8 +1,33 @@
 // HOOKS
 
+const D4input = document.getElementById("D4input");
+const D4plus = document.getElementById("D4plus");
+const D4minus = document.getElementById("D4minus");
+
 const D6input = document.getElementById("D6input");
 const D6plus = document.getElementById("D6plus");
 const D6minus = document.getElementById("D6minus");
+
+const D8input = document.getElementById("D8input");
+const D8plus = document.getElementById("D8plus");
+const D8minus = document.getElementById("D8minus");
+
+const D10input = document.getElementById("D10input");
+const D10plus = document.getElementById("D10plus");
+const D10minus = document.getElementById("D10minus");
+
+const D100input = document.getElementById("D100input");
+const D100plus = document.getElementById("D100plus");
+const D100minus = document.getElementById("D100minus");
+
+const D12input = document.getElementById("D12input");
+const D12plus = document.getElementById("D12plus");
+const D12minus = document.getElementById("D12minus");
+
+const D20input = document.getElementById("D20input");
+const D20plus = document.getElementById("D20plus");
+const D20minus = document.getElementById("D20minus");
+
 const rollResult = document.getElementById("rollResult");
 const individualResults = document.getElementById("individualResults");
 const explodingCheckbox = document.getElementById("exploding");

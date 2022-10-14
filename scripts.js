f9a86efa785ad251827cc308e743ef49a6ab2d92
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-10-14 12:23:07 +0200

    code cleanup

--- scripts.js
+++ scripts.js
@@ -1,39 +1,3 @@
-// SELECTORS
-
-// const D4input = document.getElementById("D4input");
-// const D4plus = document.getElementById("D4plus");
-// const D4minus = document.getElementById("D4minus");
-
-// const D6input = document.getElementById("D6input");
-// const D6plus = document.getElementById("D6plus");
-// const D6minus = document.getElementById("D6minus");
-
-// const D8input = document.getElementById("D8input");
-// const D8plus = document.getElementById("D8plus");
-// const D8minus = document.getElementById("D8minus");
-
-// const D10input = document.getElementById("D10input");
-// const D10plus = document.getElementById("D10plus");
-// const D10minus = document.getElementById("D10minus");
-
-// const D100input = document.getElementById("D100input");
-// const D100plus = document.getElementById("D100plus");
-// const D100minus = document.getElementById("D100minus");
-
-// const D12input = document.getElementById("D12input");
-// const D12plus = document.getElementById("D12plus");
-// const D12minus = document.getElementById("D12minus");
-
-// const D20input = document.getElementById("D20input");
-// const D20plus = document.getElementById("D20plus");
-// const D20minus = document.getElementById("D20minus");
-
-const rollResult = document.getElementById("rollResult");
-const individualResults = document.getElementById("individualResults");
-const explodingCheckbox = document.getElementById("exploding");
-
-const cardContainer = document.getElementsByClassName("container")[0];
-
 // STORING ARRAYS
 
 const diceTypes = [4, 6, 8, 10, 100, 12, 20];
@@ -45,8 +9,15 @@ for (let i = 0; i < 101; i++) {
   numberOfDice.push(0);
 }
 
-// GENERATE DICE WRAPPERS
+// SELECTORS
 
+const rollResult = document.getElementById("rollResult");
+const individualResults = document.getElementById("individualResults");
+const explodingCheckbox = document.getElementById("exploding");
+
+const cardContainer = document.getElementsByClassName("container")[0];
+
+// GENERATE DICE WRAPPERS
 
 const generateAll = function (array) {
   generateCards(array);
@@ -173,69 +144,6 @@ const rollAll = function () {
 };
 
 // BUTTONS
-
-// D4plus.addEventListener("click", (e) => {
-//   plusDice(4, D4input);
-//   e.preventDefault();
-// });
-// D4minus.addEventListener("click", (e) => {
-//   minusDice(4, D4input);
-//   e.preventDefault();
-// });
-
-// D6plus.addEventListener("click", (e) => {
-//   plusDice(6, D6input);
-//   e.preventDefault();
-// });
-// D6minus.addEventListener("click", (e) => {
-//   minusDice(6, D6input);
-//   e.preventDefault();
-// });
-
-// D8plus.addEventListener("click", (e) => {
-//   plusDice(8, D8input);
-//   e.preventDefault();
-// });
-// D8minus.addEventListener("click", (e) => {
-//   minusDice(8, D8input);
-//   e.preventDefault();
-// });
-
-// D10plus.addEventListener("click", (e) => {
-//   plusDice(10, D10input);
-//   e.preventDefault();
-// });
-// D10minus.addEventListener("click", (e) => {
-//   minusDice(10, D10input);
-//   e.preventDefault();
-// });
-
-// D100plus.addEventListener("click", (e) => {
-//   plusDice(100, D100input);
-//   e.preventDefault();
-// });
-// D100minus.addEventListener("click", (e) => {
-//   minusDice(100, D100input);
-//   e.preventDefault();
-// });
-
-// D12plus.addEventListener("click", (e) => {
-//   plusDice(12, D12input);
-//   e.preventDefault();
-// });
-// D12minus.addEventListener("click", (e) => {
-//   minusDice(12, D12input);
-//   e.preventDefault();
-// });
-
-// D20plus.addEventListener("click", (e) => {
-//   plusDice(20, D20input);
-//   e.preventDefault();
-// });
-// D20minus.addEventListener("click", (e) => {
-//   minusDice(20, D20input);
-//   e.preventDefault();
-// });
 
 roll.addEventListener("click", () => {
   rollAll();
